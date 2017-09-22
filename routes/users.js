4ac4abaf3cca9590a8272139f56e002069094671
My heart is a stereo <1105912320@qq.com>
2017-09-22 18:03:37 +0800

    node

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ connection.connect();
 connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
     if (error) throw error;
     console.log('The solution is: ', results[0].solution);
-});
+})
 connection.end();
 
 var dataSuccess = {
